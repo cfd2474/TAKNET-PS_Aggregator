8b69ec11c8dc93f14f83ddc6b58ef8473377e885
cfd2474 <m.leckliter@cfa3757.org>
2026-04-12 22:15:31 -0700

    Aggregator v1.0.364: Fixed 500 error in tunnel proxy and synchronized version.

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # =============================================================================
-# TAKNET-PS Aggregator v1.0.363 — Installer
+# TAKNET-PS Aggregator v1.0.364 — Installer
 # Target: Rocky Linux 8.x / 9.x
 #
 # Install methods:
